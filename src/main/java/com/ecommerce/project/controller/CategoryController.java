7f4805ad5fab2fcf4483ff258f83074d6cc3d081
Syedasif101 <saasif51120@gmail.com>
2025-11-24 11:21:43 +0530

    added Response Entity to better handle Status codes and body

--- src/main/java/com/ecommerce/project/controller/CategoryController.java
+++ src/main/java/com/ecommerce/project/controller/CategoryController.java
@@ -3,7 +3,10 @@ package com.ecommerce.project.controller;
 import com.ecommerce.project.model.Category;
 import com.ecommerce.project.service.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -31,15 +34,23 @@ public class CategoryController {
     }
 
     @DeleteMapping("/api/admin/categories/{categoryId}")
-    public String deleteCategory(@PathVariable Long categoryId){
+    public ResponseEntity<String> deleteCategory(@PathVariable Long categoryId){
 
-        String status = categoryService.deleteCategory(categoryId);
+        try {
 
-        return status;
+            String status = categoryService.deleteCategory(categoryId);
+            return new ResponseEntity<>(status, HttpStatus.OK);
+        }
+        catch(ResponseStatusException e)
+        {
+            return new ResponseEntity<>(e.getReason(), e.getStatusCode());
+
+        }
+
     }
 
 
-    }
+
 
 
 
